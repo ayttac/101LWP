42b39fd46eaaa5ce0ab7a3c3778b933a2d09b576
ayttac <126759113+ayttac@users.noreply.github.com>
2023-04-26 00:47:29 +0400

    work18

---
+++ 18-CreditCardDarkMode/assest/js/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const mood =document.querySelector(".mood");
+
+mood.addEventListener("click", () =>{
+    container.classList.toggle("change");
+} )
