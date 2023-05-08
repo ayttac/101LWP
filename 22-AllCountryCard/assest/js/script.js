eacacdfdffba0234822758ef6dcc8101079ccccb
ayttac <126759113+ayttac@users.noreply.github.com>
2023-05-08 11:55:03 +0400

    script.js

--- 22-AllCountryCard/assest/js/script.js
+++ 22-AllCountryCard/assest/js/script.js
@@ -74,3 +74,96 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   });
 });
 
+let showAllBtn = document.getElementById("show-all-btn");
+function showAllCards() {
+  clearCards();
+  fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(country => {
+        const name = country.name.common;
+        const flag = country.flags.svg;
+        const population = country.population;
+        const capital = country.capital ? country.capital[0] : "";
+        const currencies = country.currencies ? Object.values(country.currencies).map(c => c.name).join(", ") : "";
+        const phoneCode = "" + country.idd.root+ country.idd.suffixes;
+       
+        const card = document.createElement("div");
+        card.classList.add("card1");
+        const title = `
+          <div class="title">
+            <div class="country">Country</div>
+            <div class="flag">
+              <img src="${flag}" alt="${name} flag">
+            </div>
+            <div class="name_bar">${name}</div>
+          </div>
+        `;
+        const about = `
+          <div class="about">
+            <div class="box">
+              <div class="box1">
+                <div class="country_region">Capital</div>
+              </div>
+              <div class="box2">
+                <div class="region_name">${capital}</div>
+              </div>
+            </div>
+            <div class="box">
+              <div class="box1">
+                <div class="countries_name">Currencies</div>
+              </div>
+              <div class="box2">
+                <div class="country_name">${currencies}</div>
+              </div>
+            </div>
+            <div class="box">
+              <div class="box1">
+                <div class="currencies_text">Population</div>
+              </div>
+              <div class="box2">
+                <div class="currencies_name">${population}</div>
+              </div>
+            </div>
+            <div class="box">
+              <div class="box1">
+                <div class="machine_series_name">Phone Code</div>
+              </div>
+              <div class="box2">
+                <div class="series">${phoneCode}</div>
+              </div>
+            </div>
+          </div>
+        `;
+
+        card.innerHTML = title + about;
+        document.querySelector(".country_cards").appendChild(card);
+      });
+    });
+}
+showAllBtn.addEventListener("click", showAllCards);
+
+let showNeighborsBtn = document.getElementById("show-neighbors-btn");
+
+showNeighborsBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  showNeighbors(countryName);
+  if (countryName) {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+      .then(res => res.json())
+      .then(data => {
+        let neighbors = data[0].borders;
+        console.log(neighbors);
+        if (neighbors && neighbors.length > 0) {
+          console.log(`Neighbors of ${countryName}:`);
+          neighbors.forEach(neighbor => {
+            console.log(neighbor);
+          });
+        } else {
+          console.log(`${countryName} has no neighbors.`);
+        }
+      })
+      .catch(err => console.error(err));
+  }
+});
+
