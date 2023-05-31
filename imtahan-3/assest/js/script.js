7b5d01d5132759990b2862ffca9162d38e398039
ayttac <126759113+ayttac@users.noreply.github.com>
2023-05-31 08:41:41 +0400

    imtahan3

---
+++ imtahan-3/assest/js/script.js
@@ -0,0 +1,14 @@
+const title = document.querySelector(".title");
+const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
+const exit = document.querySelector(".exit");
+// menu.addEventListener("click",() =>{
+//     header.style.display = header.style.display === 'none' ? 'block' : 'none';
+//     title.style.display = title.style.display === 'none' ? 'block' : 'block';
+// })
+
+// exit.addEventListener("click",() =>{
+//     header.style.display = header.style.display === 'none' ? 'block' : 'block';
+//     title.style.display = title.style.display === 'none' ? 'block' : 'none';
+// })
+
